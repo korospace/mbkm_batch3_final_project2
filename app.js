d157db944b47fdcd3439038dbb16fe1b2f56952c
korospace <bagaselkoro@gmail.com>
2022-10-28 09:46:23 +0700

    edit readme

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const socialMediaRoute = require("./routers/socialMediaRoute");
 const commentRoute = require("./routers/commentRoute");
 const photoRoute = require("./routers/photoRoute");
+const swaggerDocument = require('./swagger.json');
 const userRoute = require("./routers/userRoute");
+const swaggerUi = require('swagger-ui-express');
 const port = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
@@ -12,6 +14,8 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.status(200).json({message:"final project 2 tim 3"})
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(userRoute);
 app.use(photoRoute);
--- readme.md
+++ readme.md
@@ -4,3 +4,20 @@
         <a href="https://koro-project2.up.railway.app"><strong>https://koro-project2.up.railway.app</strong></a>
     </h2>
 </p>
+<hr>
+<br>
+<br>
+
+## How to run this project at your local server
+
+1. Setup your database in /config/config.json at development section
+2. Run command line below
+    ```bash
+    npm install
+    npm run build
+    npm run start
+    ```
+3. The project will start at http://localhost:4000
+
+## API Docs
+comming soon
